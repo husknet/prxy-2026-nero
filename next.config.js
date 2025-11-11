2ddfa7e5732e90b401856615bbc59ef72e72e83f
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-11-11 09:20:09 -0500

    Refactor next.config.js to remove comments

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Generates a self-contained Node server (needed for Docker)
   output: 'standalone',
 
-  // Enable the App Router (your [[...path]] lives here)
-  experimental: {
-    appDir: true,
-  },
-
-  // Global CORS headers
   async headers() {
     return [
       {
